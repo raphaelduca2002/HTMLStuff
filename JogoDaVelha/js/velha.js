07291fb0825f2623c0b2f7a2317ae6e41c99a9a4
raphaelduca2002 <raphaelduca@hotmail.com>
2026-09-05 18:46:57 -0300

    Correção de bugs no jogo da velha

--- JogoDaVelha/js/velha.js
+++ JogoDaVelha/js/velha.js
@@ -10,8 +10,7 @@ const selecionarJogador = document.getElementById('selecionarJogador');
 const confirmarBtn = document.getElementById('confirmarbtn');
 
 let estadoTabuleiro = ["", "", "", "", "", "", "", "", ""];
-let jogadorAtual = "X";
-let jogoAtivo = true;
+let jogoAtivo = false;
 
 const combinacoesVencedoras = [
     [0, 1, 2],
@@ -66,6 +65,12 @@ function verificarResultado() {
         statusMensagem.innerHTML = `O Jogador <span>${jogadorAtual}</span> Venceu!`;
         jogoAtivo = false;
         jogosrealizados.textContent = parseInt(jogosrealizados.textContent) + 1;
+        selecionarJogador.disabled = true;
+        confirmarBtn.disabled = true;
+        btnReiniciar.disabled = false;
+        btnReiniciar.innerHTML = 'Reiniciar Jogo';
+        btnReiniciar.style.cursor = 'pointer';
+        btnReiniciar.style.backgroundColor = '#139213';
 
         if (jogadorAtual === "X") {
             vitoriaX.textContent = parseInt(vitoriaX.textContent) + 1;
@@ -83,6 +88,12 @@ function verificarResultado() {
 
         empates.textContent = parseInt(empates.textContent) + 1;
         jogosrealizados.textContent = parseInt(jogosrealizados.textContent) + 1;
+        selecionarJogador.disabled = true;
+        confirmarBtn.disabled = true;
+        btnReiniciar.disabled = false;
+        btnReiniciar.innerHTML = 'Reiniciar Jogo';
+        btnReiniciar.style.cursor = 'pointer';
+        btnReiniciar.style.backgroundColor = '#139213';
         return;
     }
 
@@ -95,21 +106,39 @@ function mudarTurno() {
 };
 
 function reiniciarJogo() {
-    jogadorAtual = "X";
     estadoTabuleiro = ["", "", "", "", "", "", "", "", ""];
-    jogoAtivo = true;
-    statusMensagem.innerHTML = `Vez do jogador: <span>${jogadorAtual}</span>`;
+    jogoAtivo = false;
+    statusMensagem.innerHTML = `Aguardando a escolha do jogador inicial`;
     reinicios.textContent = parseInt(reinicios.textContent) + 1;
 
     celulas.forEach(celula => {
         celula.textContent = "";
         celula.classList.remove('x', 'o');
     });
+
+    selecionarJogador.style.cursor = 'pointer';
+    confirmarBtn.innerHTML = 'Confirmar';
+    selecionarJogador.disabled = false;
+    confirmarBtn.disabled = false;
+    confirmarBtn.style.backgroundColor = '#139213';
+    confirmarBtn.style.cursor = 'pointer';
 };
 
 function confirmarJogador() {
     jogadorAtual = selecionarJogador.value;
     statusMensagem.innerHTML = `Vez do jogador: <span>${jogadorAtual}</span>`;
+    jogoAtivo = true;
+
+    selecionarJogador.disabled = true;
+    selecionarJogador.style.cursor = 'not-allowed';
+    confirmarBtn.disabled = true;
+    confirmarBtn.style.backgroundColor = '#cccccc';
+    confirmarBtn.style.cursor = 'not-allowed';
+    confirmarBtn.innerHTML = 'Aguardando o reinício do Jogo';
+    btnReiniciar.style.cursor = 'not-allowed';
+    btnReiniciar.disabled = true;
+    btnReiniciar.style.backgroundColor = '#cccccc';
+    btnReiniciar.innerHTML = 'Aguardando o fim da rodada';
 }
 
 
@@ -119,4 +148,4 @@ btnReiniciar.addEventListener('click', reiniciarJogo);
 
 confirmarBtn.addEventListener('click', confirmarJogador);
 
-statusMensagem.innerHTML = `Vez do jogador: <span>${jogadorAtual}</span>`;
+statusMensagem.innerHTML = `Aguardando a escolha do jogador inicial`;
